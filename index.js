216a25abb8ecb03fa584db20acbc1ffa25a47895
wang <767074895@qq.com>
2018-07-20 16:41:34 +0800

    '提交组件库雏形,已建立npm版本管理'

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Date", {
+  enumerable: true,
+  get: function get() {
+    return _date.default;
+  }
+});
+Object.defineProperty(exports, "easyScroll", {
+  enumerable: true,
+  get: function get() {
+    return _easyscroll.default;
+  }
+});
+
+var _date = _interopRequireDefault(require("./lib/date/date"));
+
+var _easyscroll = _interopRequireDefault(require("./lib/common/easyscroll"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Date from './lib/date/date';
+import easyScroll from './lib/common/easyscroll'
+export {
+    Date,
+    easyScroll
+}
